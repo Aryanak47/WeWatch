be5a1ad47e59e1f899dff66de3167460da3647d0
Aryanak47 <aryanbimali45@gmail.com>
2022-03-07 13:30:30 +0545

    add search functionality

--- ajax/getSearchResult.php
+++ ajax/getSearchResult.php
@@ -1,17 +1,61 @@
 <?php
 require_once("../includes/config.php");
 require_once("../includes/classes/FormSanitizer.php");
-require_once("../includes/classes/SearchResultsProvider.php");
 require_once("../includes/classes/EntityProvider.php");
 require_once("../includes/classes/Entity.php");
 
 if(isset($_POST["text"]) && isset($_POST["username"])) {
     $text = FormSanitizer::getSafeValue($_POST['text']);
-    $srp = new SearchResultsProvider($conn, $_POST["username"]);
-    echo $srp->getResults($text);
+    $entities = EntityProvider::getSearchEntities($conn, $text);
+    if(count($entities) > 0){
+        echo getResults($entities);
+        return;
+    }
+    // call api 
+    echo 0;
+    
 
 }
 else {
     echo "No term or username passed into file";
+}
+
+
+
+ function getResults($entities) {
+    $html = "<div class='previewCategories noScroll'>";
+    if(count($entities) < 1){
+        $html .= "<h2> No results found</h2></div>";
+        return $html;
+
+    }
+
+    $html = "<div class='previewCategories noScroll'>";
+
+    $html .= getResultHtml($entities);
+
+    return $html . "</div>";
+}
+
+ function getResultHtml($entities) {
+  
+
+    $entitiesHtml = "";
+    foreach($entities as $entity) {
+        $id = $entity->getId();
+        $thumbnail = $entity->getThumbnail();
+        $name = $entity->getName();
+        $entitiesHtml .= "<a href='entity.php?id=$id'>
+                            <div class='previewContainer small'>
+                                <img src='$thumbnail' title='$name'>
+                            </div>
+                        </a>";
+    }
+
+    return "<div class='category'>
+                <div class='entities'>
+                    $entitiesHtml
+                </div>
+            </div>";
 }
 ?>
--- search.php
+++ search.php
@@ -6,20 +6,33 @@
     <input type="text"  class="searchInput" placeholder="Search for Something...">
 </div>
 
-<div class="results"></div>
+<div class="results">
+    
+</div>
+<div class="loader" style="display: none">
+        <img src="assets/images/load.gif" alt="loader" />
+</div>
 
 
 <script>
     $(function(){
         let timer;
         let user = '<?php echo $userLoggedIn; ?>';
-
         $(".searchInput").on("keyup", function(event){
             clearTimeout(timer);
             timer = setTimeout(function(){
                 let value = $(event.target).val();
                 if(value != "") {
+                $(".loader").fadeIn();
                 $.post("ajax/getSearchResult.php", { text: value, username: user }, function(data) {
+                    if(data == 0) {
+                        $(".results").html("");
+                        console.log("calling api")
+                        load_details(value)
+                        return
+
+                    }
+                    $(".loader").delay(300).fadeOut();
                     $(".results").html(data);
                 })
             }
@@ -31,7 +44,56 @@
 
             },2000)
         })
+        function load_details(title){
+            $.ajax({
+                type: 'GET',
+                url:`https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query=${title}`,
+
+                success: function(movie){
+                if(movie.results.length<1){
+                    let html = "<div class='previewCategories noScroll'><h2> No results found</h2></div>"
+                    $(".results").html(html);
+                    $(".loader").delay(300).fadeOut();
+                }
+                else{
+                    console.log(movie.results)
+                   
+                    let movies = movie.results
+                    let html = `<div class="previewCategories noScroll">
+                                    <div class="category">
+                                        <div class="entities">`
+                    movies.forEach(m => {
+                        let title = m.title
+                        let thumbnail = m.poster_path
+                        let id = m.id
+                        if(title && thumbnail &&id){
+                            html += getHtmlContainer(title,id,thumbnail)
+
+                        }
+                    });
+                    html += ` </div></div> </div>`
+                    $(".loader").delay(300).fadeOut();
+                    $(".results").html(html);
+                }
+                },
+                error: function(){
+                alert('Invalid Request');
+                $(".loader").delay(300).fadeOut();
+                },
+            });
+        }
+        function getHtmlContainer(title,id,thumbnail){
+            var poster = `https://image.tmdb.org/t/p/original${thumbnail}`;
+            let html = `
+                        <a href="apimovie.php?id=1">
+                            <div class="previewContainer small">
+                                <img src="${poster}" title="${title}" alt="${title}" />
+                            </div>
+                        </a>
+                  `
+            return html;
+
+        }
     })
-
-
+    
 </script>
